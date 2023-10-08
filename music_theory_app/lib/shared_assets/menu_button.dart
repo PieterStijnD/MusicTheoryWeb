d34d7993172c3322a11b02063d109c433b69f504
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-10-08 09:54:55 +0200

    set up general tile with parameters

--- music_theory_app/lib/shared_assets/menu_button.dart
+++ music_theory_app/lib/shared_assets/menu_button.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MenuButton extends StatelessWidget {
+class MenuButton extends StatefulWidget {
+  final String listTileName;
+  final Icon icon;
+
   const MenuButton({
     super.key,
+    required this.listTileName,
+    required this.icon,
   });
 
   @override
+  State<MenuButton> createState() => _MenuButtonState();
+}
+
+class _MenuButtonState extends State<MenuButton> {
+  @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        debugPrint("ListTile");
-      },
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.list),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("ListTile"),
-          ),
-        ],
+    final listTileName = widget.listTileName;
+    final icon = widget.icon;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          debugPrint("$listTileName pressed");
+        },
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: icon,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(listTileName),
+            ),
+          ],
+        ),
       ),
     );
   }
