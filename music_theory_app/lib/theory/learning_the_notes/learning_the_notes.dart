a2ce3c56daf9d6a10484e15203a1ef6971e34da5
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-10-10 17:46:18 +0200

    learning the notes base page

---
+++ music_theory_app/lib/theory/learning_the_notes/learning_the_notes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LearningTheNotes extends StatelessWidget {
+  const LearningTheNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
