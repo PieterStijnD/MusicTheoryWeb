40285908d8e383fd69710d75dbc40a1e0e1ecdeb
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-10-10 17:46:38 +0200

    rythms placeholder page

---
+++ music_theory_app/lib/theory/learning_the_notes/rythms.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Rythms extends StatelessWidget {
+  const Rythms({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
